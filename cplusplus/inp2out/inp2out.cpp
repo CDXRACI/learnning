dd540df5eaa0015c3ba62e10df4a541c43de3ea7
phuccaodai <phuc-cao.dai@banvien.com>
2023-10-14 11:48:51 +0700

    added inp2out

---
+++ cplusplus/inp2out/inp2out.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main(){
+        int l_val = 0;
+        cout <<" Hello world\r\n" << endl;
+        cin >> l_val;
+        cout << " value that is typing... " << l_val << endl;
+}
