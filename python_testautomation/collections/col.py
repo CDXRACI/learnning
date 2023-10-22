bd6a267db6361f63f120111e59c91db0affc0575
phuccaodai <phuc-cao.dai@banvien.com>
2023-10-22 23:46:01 +0700

    added collection

---
+++ python_testautomation/collections/col.py
@@ -0,0 +1,19 @@
+# collection as array
+hello = ["hello1","hello2", "hello3"]
+print(hello[2])
+print(hello[0:3])
+#for
+for hellox in hello:
+    print(hellox) 
+
+#dicionary as structure
+confis = {
+    "browser":"opera",
+    "aut":"google site",
+    "test": "smoke",
+    "log": True
+}
+
+print(confis.get("aut"))
+for var1 in confis.values():
+    print(var1)
