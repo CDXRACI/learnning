ce0d1c106e5b46ddcd9cc3d4cf2922294e2a8329
phuccaodai <phuc-cao.dai@banvien.com>
2023-10-23 22:02:51 +0700

    added input python

--- python_testautomation/variables/first.py
+++ python_testautomation/variables/first.py
@@ -1,3 +1,4 @@
+import math 
 #variable
 name = "cao_dai_phucd"
 salary = 1000
@@ -14,3 +15,34 @@ print ( " salary1 ", salary1 )
 #datatype
 print("--> Hello ", type( name ))
 print("--> Hello ", type( salary ))
+#print with specific character
+print("hello\ncdp") # using \ not use \"
+print(len(name))
+print(name.find('a'))
+print(name.lower())
+print(name.upper())
+#is behind check correct or not correct
+print(name.isupper()) 
+print(name.islower())
+print(name[2])
+
+#------------------NUMBER-----------------------------
+fl_var: float = 0.222
+in_var: int  = 123
+in_var2: int = 34
+print(fl_var)
+print((in_var*in_var2))
+print(" hello world", in_var)
+print(10%3)
+print(abs(-2))
+print(pow(3,2))
+print(math.sqrt(4))
+print(max(2,5))
+print(min(1,3))
+print(round(3.812, 3))
+#print("hello world "  +  in_var ) // wrong
+
+#---------------------Input-----------------
+var_str1: str = input("please typing in the input: ")
+var_str2: str = input("enter your name: ")
+print("hello" + var_str1 + " xin chao" + var_str2 )
